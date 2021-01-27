7bfd7ad2d013ad1c73ececf8f2372e98e445810f
Flourishya <35487252+Flourishya@users.noreply.github.com>
2021-01-27 18:30:41 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -7,7 +7,7 @@ struct Stats
     float max = 0;
 };
 
-void compute_statistics(const float* numberset, int setlength);
+struct Stats compute_statistics(const float* numberset, int setlength);
 
 typedef void (*alerter_funcptr)();
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
